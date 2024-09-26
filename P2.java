476e769d1d8762114fc39df12e75575216a40d1c
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2024-09-26 09:58:16 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,12 @@
+public class P2{
+public static void main(String[] args){
+String s1="ObjectOriented";
+String s2="";
+for(int i=1;i<=14;i++){
+System.out.printf("%"+(14+i)/2+"s%n",s1.substring(0,i));
+}
+for(int i=14-1;i>0;i--){
+System.out.printf("%"+9(14+i)/2+"s%n",s1.substring(0,i));
+}
+}
+}
