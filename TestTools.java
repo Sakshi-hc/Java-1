d50b0f1e12197738bce2dba0dec4bfd643b98ecd
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2024-11-14 10:25:02 +0530

    Add files via upload

---
+++ TestTools.java
@@ -0,0 +1,16 @@
+
+package pkg1;
+
+public class TestTools {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+    t1.numbers(10);
+    t1.even(10);
+    t1.odd(10);
+    t1.mul3(10);
+    t1.mul4(10);
+    t1.mul5(10);
+    }
+    
+    
+}
