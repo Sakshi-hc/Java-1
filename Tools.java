59d73c12341ed1b428805d078007e155a07cc156
Sakshi-hc <sakshi.23cs130@sode-edu.in>
2024-11-14 10:24:49 +0530

    Add files via upload

---
+++ Tools.java
@@ -0,0 +1,43 @@
+package pkg1;
+
+public class Tools {
+    
+   public void numbers(int n1){
+        for(int i=0;i<=n1;i++){ 
+       System.out.print(i+" ");
+  }
+        System.out.println();
+   }
+    public void even(int n1){
+     for(int i=0;i<=n1;i++){
+       System.out.print(i*2+" ");
+     }
+     System.out.println();
+   }
+public void odd(int n1){
+    for(int i=0;i<=n1;i++){
+        System.out.print(i*2+1+" "); 
+}
+    System.out.println();
+}
+public void mul3(int n1){
+    
+    for(int i=1;i<=n1;i++){
+       System.out.print(i*3+" ");
+       }
+    System.out.println();
+}
+ public void mul4(int n1){
+    for(int i=1;i<=n1;i++){
+        System.out.print(i*4+" ");
+    }
+    System.out.println();
+   }
+  public void mul5(int n1){
+   
+    for(int i=1;i<=n1;i++){
+        System.out.print(i*5+" ");
+}
+    System.out.println();
+}
+}
